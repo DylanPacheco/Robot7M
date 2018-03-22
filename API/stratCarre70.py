1761f176ba1454bc87b7da8619480367e775b941
Dylan <dyla.pacheco@gmail.com>
2018-03-22 12:11:53 +0100

    Update stratCarre70.py

--- API/stratCarre70.py
+++ API/stratCarre70.py
@@ -4,26 +4,26 @@ import robot2I013
 
 class StratCarre70():
     
-    def __init__(robot):
+    def __init__(self,robot):
         self.distance=0
         self.cpt=0
-        self.stop=False
+        self.stp=False
         self.robot=robot
-        self.S70=Strat70(robot)
-        self.SRot=StratRotD90(robot)
+        self.S70=strat70.Strat70(robot)
+        self.SRot=stratRotD90.StratRotD90(robot)
         
-    def update():
+    def update(self):
         if not self.S70.stop():
             self.S70.update()
         else:
             if not self.SRot.stop():
                 self.SRot.update()
             else:
-                self.S70=Strat70(self.robot)
+                self.S70=strat70.Strat70(self.robot)
                 self.cpt+=1
-                self.SRot=StratRotD90(self.robot)
+                self.SRot=stratRotD90.StratRotD90(self.robot)
         if self.cpt == 4:
-            self.stop=True
+            self.stp=True
             
-    def stop():
-        return self.stop
+    def stop(self):
+        return self.stp
