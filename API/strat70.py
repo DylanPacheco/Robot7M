ca512dd53711f227102550ff28e8b4c48a896df8
Dylan <dyla.pacheco@gmail.com>
2018-03-29 10:46:48 +0200

    Add files via upload

--- API/strat70.py
+++ API/strat70.py
@@ -1,29 +1,25 @@
 import robot2I013
 
-class StratRotD90():
+class Strat70():
     
     def __init__(self,robot):
-        self.rot=0
+        self.distance=0
         self.stp=False
         self.robot=robot
-        self.vitessed=-50
-        self.vitesseg=50
+        self.vitesse=200
         self.prec=self.robot.get_motor_position()[0]
         self.suiv=self.robot.get_motor_position()[0]
         
     def update(self):
         self.prec=self.robot.get_motor_position()[0]
-        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT,-200)
-        self.robot.set_motor_dps(self.robot.MOTOR_LEFT,200)
+        self.robot.set_motor_dps(self.robot.MOTOR_LEFT+self.robot.MOTOR_RIGHT,self.vitesse)
         self.suiv=self.robot.get_motor_position()[0]
-        self.rot+=self.suiv-self.prec
-        print(self.rot)
-        if self.rot >= 105:
+        self.distance+=(self.suiv-self.prec)*self.robot.WHEEL_CIRCUMFERENCE/360.0
+        if self.distance > 700-self.robot.WHEEL_CIRCUMFERENCE:
+            self.vitesse=100
+        if self.distance >= 700:
             self.stp=True
             self.robot.set_motor_dps(self.robot.MOTOR_LEFT+self.robot.MOTOR_RIGHT,0)
-        if self.rot >= 75:
-            self.vitessed=-30
-            self.vitesseg=30
         
     def stop(self):
         return self.stp
--- API/testRobot.py
+++ API/testRobot.py
@@ -5,6 +5,10 @@ import robot2I013
 import simulation
 
 robot=robot2I013.Robot2I013()
+
+#strat=strat70.Strat70(robot)
 strat=stratCarre70.StratCarre70(robot)
+#strat=stratRotD90.StratRotD90(robot)
+
 simul=simulation.Simulation(strat)
 simul.run()
